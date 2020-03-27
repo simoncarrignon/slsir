c7136ba8212028efcaf74c27a53d24c8b96dcfe2
Simon <simon@elisya.org>
2020-03-27 21:12:25 +0100

    update comment and visualisation graph

--- abmEpi.R
+++ abmEpi.R
@@ -13,11 +13,11 @@ names(sir)=c("S","I","R")
 #' @param di the distance between to agent under which the disease cna be transmitted
 #' @param i0 the number of initial infection
 #' @param speed the speed of the agents
-#' @param remi time for agent to pass form infected to R
+#' @param reco time for agent to Recover
 #' @param visu TRUE or FALSE, if output should be plotted
 #' @param sat speed of saturation of the sigmoid
 #' @param inf inflexion point of the sigmoid
-abmSIR <- function(pop,tstep,p=1,i0=1,di=2,remi=10,speed=.8,xsize=100,ysize=100,visu=FALSE,inf=.5,sat=10){
+abmSIR <- function(pop,tstep,p=1,i0=1,di=2,reco=10,speed=.8,xsize=100,ysize=100,visu=FALSE,inf=.5,sat=10){
     
     if(is.null(dim(pop))) #if pop is a unique number (ie not preinitialized) 
         pop=generatePopulation(N=pop)
@@ -104,6 +104,6 @@ sig<-function(x,a=10,b=.5)1/(1+exp(-a*(x-b)))
 visualize <- function(pop,timeseries){
             par(mfrow=c(1,2))
             plot(pop[,"x"],pop[,"y"],pch=21,bg=pop[,"health"]-1,ylim=c(0,ysize),lwd=.2,xlim=c(0,xsize),xlab="",ylab="")
-            plot(1:t,timeseries[,2],col="red",type="l",ylim=c(0,N),xlab="time",ylab="# infected")
-            lines(1:t,timeseries[,5],col="blue",type="l",ylim=c(0,N),xlab="time",ylab="# GOOD")
+            plot(1:nrow(timeseries),timeseries[,2],col="red",type="l",ylim=c(0,N),xlab="time",ylab="# infected")
+            lines(1:nrow(timeseries),timeseries[,5],col="blue",type="l",ylim=c(0,N),xlab="time",ylab="# GOOD")
 }
