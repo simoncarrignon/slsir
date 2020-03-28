3450e13d1b021bc3549ff905b1908a6ecdd01815
Simon Carrignon <simon@nimbios.org>
2020-03-28 15:51:30 -0400

    paralel run

--- exploreModel.R
+++ exploreModel.R
@@ -1,6 +1,7 @@
+source("abmEpi.R")
 
 
-poptest=generatePopulation(500,1,1) 
+poptest=generatePopulation(500) 
 neutral=replicate(50,abmSIR(poptest,2500,speed=.6,p=c(1,1),di=2,i0=1,visu=F)$timeseries[,2])
 twiceless=replicate(50,abmSIR(poptest,2500,speed=.6,p=c(1,.5),di=2,i0=1,visu=F,inf=.5)$timeseries[,2])
 tenless=replicate(50,abmSIR(poptest,2500,speed=.6,p=c(1,.1),di=2,i0=1,visu=F,inf=.5)$timeseries[,2])
@@ -9,9 +10,12 @@ neutral=apply(replicate(50,abmSIR(poptest,1500,speed=.6,p=c(1,1),di=2,i0=1,visu=
 twiceless=apply(replicate(50,abmSIR(poptest,1500,speed=.6,p=c(1,.5),di=2,i0=1,visu=F,inf=.5)$timeseries[,2]),1,mean)
 tenless=apply(replicate(50,abmSIR(poptest,1500,speed=.6,p=c(1,.1),di=2,i0=1,visu=F,inf=.5)$timeseries[,2]),1,mean)
 
-inpoint=seq(.1,.9,.1)
-twiceless=sapply(inpoint,function(i)apply(replicate(20,abmSIR(poptest,1500,speed=.6,p=c(1,.5),di=2,i0=1,visu=F,inf=i)$timeseries[,2]),1,mean))
-tenless=sapply(inpoint,function(i)apply(replicate(20,abmSIR(poptest,1500,speed=.6,p=c(1,.1),di=2,i0=1,visu=F,inf=i)$timeseries[,2]),1,mean))
+inpoint=seq(0,1,.1)
+library(parallel)
+cl <- makeForkCluster(40,outfile="")
+twiceless=sapply(inpoint,function(i)apply(parSapply(cl,1:300,function(j){print(j);abmSIR(poptest,1500,speed=.6,p=c(1,.5),di=2,i0=1,visu=F,inf=i,sat=50)}$timeseries[,2]),1,mean))
+tenless=sapply(inpoint,function(i)apply(parSapply(cl,1:300,function(j){print(j);abmSIR(poptest,1500,speed=.6,p=c(1,.1),di=2,i0=1,visu=F,inf=i,sat=50)}$timeseries[,2]),1,mean))
+stopCluster(cl)
 
 cols=heat.colors(length(inpoint))
 names(cols)=as.character(inpoint)
