f5a0fe102c681fc59feb42218269b896b82ab733
Simon Carrignon <simon@nimbios.org>
2020-04-28 02:49:19 -0400

    add repetworst in graphpapergenreation

--- graphPaper.R
+++ graphPaper.R
@@ -63,12 +63,90 @@ library(parallel)
 
 save(file="worst.bin",worst)
 save(file="best.bin",best)
+
+
+
 load("best.bin")
-cl <- makeForkCluster(3,outfile="")
-neutralGood=parSapply(cl,1:100,function(i){print(i);abmSIR(poptest,1500,p=c(.1,.1),di=2,i0=1,inf=9,sat=1000,xsize=xsize,ysize=ysize,visu=F,ap=F,ts=T,file=F,p_i=1,log=F)$timeseries[,2]})
-neutralBad=parSapply(cl,1:100,function(i){n=sample(nrow(best),1);print(paste(i,n));abmSIR(poptest,1500,p=c(1,1),di=2,i0=1,inf=best$inf[n],pind=best$pind[n],inf_r=best$inf_r[n],sat_r=best$sat_r[n],sat=best$sat[n],xsize=xsize,ysize=ysize,visu=F,ap=F,ts=T,file=F,p_i=1,log=F)$timeseries[,2]})
-repetBest=sapply(1:100,function(i){print(i);abmSIR(poptest,1500,p=c(1,.2),di=2,i0=1,inf=.2,sat=1,xsize=xsize,ysize=ysize,visu=F,ap=F,ts=T,file=F,p_i=.5,log=F)$timeseries[,2]})
+load("worst.bin")
+xsize=ysize=100
+library(parallel)
+cl <- makeForkCluster(6,outfile="")
+neutralGood=parSapply(cl,1:500,function(i){
+                      print(i);
+                         simu=abmSIR(500,1500,p=c(1,1),di=2,i0=1,recovery=c(8,14)*25,speed=c(1,.2),xsize=xsize,ysize=ysize,
+                                     inf=9,
+                                     sat=1000,
+                                     p_i=1,
+                                     strategy="random",
+                                     ts=T,ap=F,visu=F
+                                     )$timeseries[,2]
+
+})
+save(file="neutralGood.bin",neutralGood)
+
+neutralBad=parSapply(cl,1:500,function(i){
+                     print(i);
+                     simu=abmSIR(500,1500,p=c(.1,.1),di=2,i0=1,recovery=c(8,14)*25,speed=c(1,.2),xsize=xsize,ysize=ysize,
+                                 inf=9,
+                                 sat=1000,
+                                 p_i=1,
+                                 strategy="random",
+                                 ts=T,ap=F,visu=F
+                                 )$timeseries[,2]
+
+})
+save(file="neutralBad.bin",neutralBad)
+
+
+repetBest=parSapply(cl,1:500,function(i){
+                    j=sample(nrow(best),1);print(paste(i,j));
+                    simu=abmSIR(500,1500,p=c(1,.1),di=2,i0=1,recovery=c(8,14)*25,speed=c(1,.2),xsize=xsize,ysize=ysize,
+                                inf=best$inf[j],
+                                sat=best$sat[j],
+                                inf_r=best$inf_r[j],
+                                sat_r=best$sat_r[j],
+                                p_i=best$pind[j],
+                                sl_rad=best$sl_rad[j],
+                                strategy="random",
+                                ts=T,ap=F,visu=F
+                                )$timeseries[,2]
+})
+save(file="repetBest.bin",repetBest)
+
+repetWorst=parSapply(cl,1:500,function(i){
+                    j=sample(nrow(worst),1);print(paste(i,j));
+                    simu=abmSIR(500,1500,p=c(1,.1),di=2,i0=1,recovery=c(8,14)*25,speed=c(1,.2),xsize=xsize,ysize=ysize,
+                                inf=worst$inf[j],
+                                sat=worst$sat[j],
+                                inf_r=worst$inf_r[j],
+                                sat_r=worst$sat_r[j],
+                                p_i=worst$pind[j],
+                                sl_rad=worst$sl_rad[j],
+                                strategy="random",
+                                ts=T,ap=F,visu=F
+                                )$timeseries[,2]
+})
+save(file="repetWorst.bin",repetWorst)
+
+
 stopCluster(cl)
+pop=generatePopulation(N=500,xsize=xsize,ysize=ysize,recovery=c(8,14)*25,speed=c(1,.2),behavior=rep(G,500))
+cl <- makeForkCluster(6,outfile="")
+neutralGoodG=parSapply(cl,1:500,function(i){
+                      print(i);
+                         simu=abmSIR(1500,p=c(.1,.1),di=2,i0=1,xsize=xsize,ysize=ysize,
+                                     pop=pop,
+                                     inf=9,
+                                     sat=1000,
+                                     p_i=1,
+                                     strategy="random",
+                                     ts=T,ap=F,visu=F
+                                     )$timeseries[,2]
+
+})
+save(file="neutralGoodG.bin",neutralGoodG)
+stopCluster(cl)
+
 
 neutralbadList=lapply(1:nrow(neutralbad),function(i)neutralbad[i,])   
 neutralGoodList=lapply(1:nrow(neutralGood),function(i)neutralGood[i,])   
