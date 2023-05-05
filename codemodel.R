d24fba7f22a64a1e8f4352b81dadd7456ab04184
Simon <simon@elisya.org>
2023-05-05 09:57:56 +0100

    standardise distances

--- codemodel.R
+++ codemodel.R
@@ -85,17 +85,20 @@ diffuse_culture <- function(g, prob_diffuse,nvisits=1,contagious_period=10) {
   return(g)
 }
 
-n=200
+n=3000
 
 #let's draw 3 maps
 
-scenarios <- list( createFringePoints(.3,n,r=1.5), createPolyPoints(h=3,n=n), createPolyPoints(h=1.5,n=n))
+scenarios <- list( createFringePoints(.3,n,r=3), createPolyPoints(h=3,n=n), createPolyPoints(h=1.5,n=n))
 
 par(mfrow=c(1,3))
 for(i in 1:3){
-    plot(0,0,xlim=c(-2.5,2.5),ylim=c(-2.5,2.5),type="n",ann=F,axes=F)
+    plot(0,0,xlim=c(-3.1,3.1),ylim=c(-3.1,3.1),type="n",ann=F,axes=F)
     plot(scenarios[[i]],add=T,pch=21,bg="red")
 }
+par(mar=c(0,0,0,0),oma=c(0,0,0,0))
+plot(0,0,xlim=c(-3.1,3.1),ylim=c(-3.1,3.1),type="n",ann=F,axes=F)
+lapply(1:3,function(i)plot(st_convex_hull(st_union(scenarios[[i]])),col=adjustcolor(categorical_pal(3)[i],.9),add=T))
 
 
 # convert these maps as graph where we will run the simulation:
